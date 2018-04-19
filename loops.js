8f7cb82f2c9c6b49a77cc0f29b97409d6e3984c3
Christian Williams <williamschristian1521@gmail.com>
2018-04-19 01:34:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,1 +1,14 @@
 function forLoop(array)
+{
+  for (let i = 0; i < 25; i++)
+  {
+    if (i !== 1)
+    {
+      array += 'I am ${i} strange loops.'
+    }
+    else
+    {
+      array += 'I am 1 strange loop.'
+    }
+  }
+}
